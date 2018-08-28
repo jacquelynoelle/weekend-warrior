9492b185dade369c9a52140551c9aae0cbe2f084
jackie <jacquelyn.cheng@me.com>
2018-08-28 09:16:22 -0700

    More dry

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -6,8 +6,8 @@ class Robot
   end
 
   def reset
-    robot_name = (0...2).map { (65 + rand(26)).chr }.join
-    robot_name << (0...3).map { rand(10) }.join
+    robot_name = (0..1).map { (65 + rand(26)).chr }.join
+    robot_name << (0..2).map { rand(10) }.join
     @name = robot_name
   end
 end
