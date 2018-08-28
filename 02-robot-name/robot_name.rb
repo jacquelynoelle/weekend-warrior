3e3afa408da63d93e7b6ab040f97eeb3c9d07c9b
jackie <jacquelyn.cheng@me.com>
2018-08-28 09:11:31 -0700

    Drying it up

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,17 +1,13 @@
 class Robot
-  attr_accessor :name
+  attr_reader :name
 
   def initialize
-    @name = generate_name
+    reset
   end
 
   def reset
-    @name = generate_name
-  end
-
-  def generate_name
     robot_name = (0...2).map { (65 + rand(26)).chr }.join
     robot_name << (0...3).map { rand(10) }.join
-    return robot_name
+    @name = robot_name
   end
 end
