0c7fd1a381120ebd33c76b63fdeba6b040320eea
jackie <jacquelyn.cheng@me.com>
2018-09-14 16:01:26 -0700

    Completed 04-word-count

--- 04-word-count/word_count.rb
+++ 04-word-count/word_count.rb
@@ -1,1 +1,11 @@
-
+def words(sentence)
+  tally = {}
+  sentence.split(" ").each do |word|
+    if tally[word.to_s]
+      tally[word.to_s] += 1
+    else
+      tally[word.to_s] = 1
+    end
+  end
+  return tally
+end
